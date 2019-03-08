710eeba1ecb9f2a7e8be6cc57aa7d2c5bd8ac110
hututudor <hutu.tudor@yahoo.com>
2019-03-08 10:59:33 +0200

    Removed build folder from gitignore

---
+++ build/precache-manifest.0c30e7897aaff818e6f8be51e7530fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "426f405b2c4d1d50a1df",
+    "url": "/static/js/main.426f405b.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "426f405b2c4d1d50a1df",
+    "url": "/static/css/main.8d374bd5.chunk.css"
+  },
+  {
+    "revision": "e45168c6347f13de5034b349d83f7bcb",
+    "url": "/index.html"
+  }
+];
