dbc1dae954839d14dc27fd0590c62d8a0ca6a6bf
hututudor <hutu.tudor@yahoo.com>
2019-03-21 21:24:22 +0200

    Added search functionality

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-import { products } from './prod.json';
+import React, {Component} from 'react';
+import {products} from './prod.json';
 import Product from './hoc/Prod.jsx';
-import { Card, Container } from 'semantic-ui-react';
+import {Card, Container} from 'semantic-ui-react';
 
 class Products extends Component {
+  state = {
+    products: products,
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+      products: products.filter(prod =>
+        prod.name.toLowerCase().includes(e.target.value.toLowerCase()),
+      ),
+    });
+  };
+
   render() {
     return (
       <div className="products">
         <h1>Produsele noastre</h1>
-        <Container>
+        <Container className="prodcontainer">
+          <div className="input">
+            <input
+              placeholder="Cauta ..."
+              value={this.state.value}
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
           <Card.Group centered stackable itemsPerRow={3}>
-            {products.map(prod => (
+            {this.state.products.map(prod => (
               <Product prod={prod} />
             ))}
           </Card.Group>
